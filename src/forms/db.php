d4501b39dbe8e5de4fcc9007780ca7f43d7e3305
Praful Patel <85041307+prafulpatel16@users.noreply.github.com>
2022-04-06 14:34:14 -0600

    Update db.php

--- src/forms/db.php
+++ src/forms/db.php
@@ -1,13 +1,13 @@
 <?php
 //$filename = 'contactus.sql';
-getenv('MYSQL_DBHOST') ? $db_host=getenv('MYSQL_DBHOST') : $db_host="localhost";
+getenv('MYSQL_DBHOST') ? $db_host=getenv('MYSQL_DBHOST') : $db_host="127.0.0.0";
 getenv('MYSQL_DBPORT') ? $db_port=getenv('MYSQL_DBPORT') : $db_port="3306";
 getenv('MYSQL_DBUSER') ? $db_user=getenv('MYSQL_DBUSER') : $db_user="root";
 getenv('MYSQL_DBPASS') ? $db_pass=getenv('MYSQL_DBPASS') : $db_pass="";
 getenv('MYSQL_DBNAME') ? $db_name=getenv('MYSQL_DBNAME') : $db_name="empdb";
 
 /* Connect to MySQL and select the database. */
-$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+$connection = mysqli_connect($db_host, $db_user, $db_pass);
 
 if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
